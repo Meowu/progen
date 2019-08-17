a23ca7bce10fa0edae0915bb338d3657ea655407
Meowu <474384902@qq.com>
2019-08-17 18:10:10 +0800

    parse process arguments.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
+import arg from 'arg'
 
-export const run = (args?: string) => {
-  console.log('args', args)
+const parseArgs = (rawArgs: string[]) => {
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes',
+      '-i': '--install'
+    },
+    {
+      argv: rawArgs.slice(2)
+    }
+  )
+  return {
+    skipPrompts: args['--yes'] || false,
+    git: args['--git'] || false,
+    template: args._[0],
+    runInstall: args['--install'] || false
+  }
 }
 
-run('233')
+export const run = (args: string[]) => {
+  const options = parseArgs(args)
+  console.log('option', options)
+}
